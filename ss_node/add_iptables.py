dbb7e82bdb8661bd0d2d45df68126cbdb8b79eb8
GameXG <GameXG@gmail.com>
2014-11-19 15:08:15 +0800

    为 iptables 增加说明。

--- ss_node/add_iptables.py
+++ ss_node/add_iptables.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # utf-8 中文编码
+u""" 为流量统计功能增加 iptables 规则
+
+实测发现在搬瓦工 VPS 上面添加150多个端口的规则时出现 "iptables: Memory allocation problem." 错误.
+DO VPS上面添加了500个端口的规则没有任何问题。可能 Openvz VPS 有限制。
+
+记录个命令
+iptables-save  > /etc/iptables-save
+iptables-restore < /etc/iptables-save
+
+"""
 import os
+
+# 清空 iptables 规则
+#os.system('iptables -F')
 
 def add_iptables():
     for i in range(8000,8500):
